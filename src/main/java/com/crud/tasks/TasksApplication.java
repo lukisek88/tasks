d191c7222fc9d38ce1880f1ccca429abe2224864
Lukasz Kudla <lukisek88@gmail.com>
2022-11-03 21:22:28 +0100

    19.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,9 +3,11 @@ package com.crud.tasks;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         /* TaskDto taskDto = new TaskDto(
@@ -22,5 +24,8 @@ public class TasksApplication {
          */
         SpringApplication.run(TasksApplication.class, args);
     }
-
+@Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+        return application.sources(TasksApplication.class);
+}
 }
