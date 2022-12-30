39b13beb33ed51a0feb889fdd2f6e840023e86fb
Lukasz Kudla <lukisek88@gmail.com>
2022-12-30 23:03:30 +0100

    22.3 i 22.4

--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -2,11 +2,16 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloListDto {
+public class TrelloListDto{
 
     @JsonProperty("id")
     private String id;
