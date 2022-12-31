be30aed3055e9d2b1f4e78af33669a5bce53bb1d
Lukasz Kudla <lukisek88@gmail.com>
2022-12-31 11:46:36 +0100

    23.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,22 +1,25 @@
 package com.crud.tasks;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//war
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@OpenAPIDefinition
 @SpringBootApplication
-
 public class TasksApplication  // extends SpringBootServletInitializer {
 {
-
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
-    } /*
+    }
+    /*
 @Override
     protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
         return application.sources(TasksApplication.class);
 } */
+
 }
